d39bdbf0a8f631097fcd4accde83740ab2fd24cc
M-Shahbaz <mr.shahbaz.aslam@gmail.com>
2021-06-22 18:57:16 +0500

    Nextjs material kit ready

--- pages/_app.js
+++ pages/_app.js
@@ -1,31 +1,93 @@
 import { Provider } from 'next-auth/client'
 import '@fontsource/roboto';
-import './styles.css'
+import './styles.css';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "next/app";
+import Head from "next/head";
+import Router from "next/router";
+
+import PageChange from "components/PageChange/PageChange.js";
+
+import "styles/scss/nextjs-material-kit.scss?v=1.2.0";
+
+
+Router.events.on("routeChangeStart", (url) => {
+  console.log(`Loading: ${url}`);
+  document.body.classList.add("body-page-transition");
+  ReactDOM.render(
+    <PageChange path={url} />,
+    document.getElementById("page-transition")
+  );
+});
+Router.events.on("routeChangeComplete", () => {
+  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
+  document.body.classList.remove("body-page-transition");
+});
+Router.events.on("routeChangeError", () => {
+  ReactDOM.unmountComponentAtNode(document.getElementById("page-transition"));
+  document.body.classList.remove("body-page-transition");
+});
+
 
 // Use the <Provider> to improve performance and allow components that call
 // `useSession()` anywhere in your application to access the `session` object.
-export default function App ({ Component, pageProps }) {
-  return (
-    <Provider
-      // Provider options are not required but can be useful in situations where
-      // you have a short session maxAge time. Shown here with default values.
-      options={{
-        // Client Max Age controls how often the useSession in the client should
-        // contact the server to sync the session state. Value in seconds.
-        // e.g.
-        // * 0  - Disabled (always use cache value)
-        // * 60 - Sync session state with server if it's older than 60 seconds
-        clientMaxAge: 0,
-        // Keep Alive tells windows / tabs that are signed in to keep sending
-        // a keep alive request (which extends the current session expiry) to
-        // prevent sessions in open windows from expiring. Value in seconds.
-        //
-        // Note: If a session has expired when keep alive is triggered, all open
-        // windows / tabs will be updated to reflect the user is signed out.
-        keepAlive: 0
-      }}
-      session={pageProps.session} >
-      <Component {...pageProps} />
-    </Provider>
-  )
+export default class MyApp extends App {
+
+  componentDidMount() {
+    let comment = document.createComment(`
+
+=========================================================
+* NextJS Material Kit v1.2.0 based on Material Kit Free - v2.0.2 (Bootstrap 4.0.0 Final Edition) and Material Kit React v1.8.0
+=========================================================
+
+* Product Page: https://www.creative-tim.com/product/nextjs-material-kit
+* Copyright 2021 Creative Tim (https://www.creative-tim.com)
+* Licensed under MIT (https://github.com/creativetimofficial/nextjs-material-kit/blob/main/LICENSE.md)
+
+* Coded by Creative Tim
+
+=========================================================
+
+* The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+
+`);
+    document.insertBefore(comment, document.documentElement);
+  }
+  static async getInitialProps({ Component, router, ctx }) {
+    let pageProps = {};
+
+    if (Component.getInitialProps) {
+      pageProps = await Component.getInitialProps(ctx);
+    }
+
+    return { pageProps };
+  }
+  render() {
+    const { Component, pageProps } = this.props;
+
+    return (
+      <Provider
+        // Provider options are not required but can be useful in situations where
+        // you have a short session maxAge time. Shown here with default values.
+        options={{
+          // Client Max Age controls how often the useSession in the client should
+          // contact the server to sync the session state. Value in seconds.
+          // e.g.
+          // * 0  - Disabled (always use cache value)
+          // * 60 - Sync session state with server if it's older than 60 seconds
+          clientMaxAge: 0,
+          // Keep Alive tells windows / tabs that are signed in to keep sending
+          // a keep alive request (which extends the current session expiry) to
+          // prevent sessions in open windows from expiring. Value in seconds.
+          //
+          // Note: If a session has expired when keep alive is triggered, all open
+          // windows / tabs will be updated to reflect the user is signed out.
+          keepAlive: 0
+        }}
+        session={pageProps.session} >
+        <Component {...pageProps} />
+      </Provider>
+    )
+  }
 }

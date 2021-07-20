8c87d57f4f37d79d271b9c39160a3c796190abf6
M-Shahbaz <mr.shahbaz.aslam@gmail.com>
2021-07-20 19:11:20 +0500

    location id reader string typo fix

--- php/src/Domain/Location/Repository/LocationReaderRepository.php
+++ php/src/Domain/Location/Repository/LocationReaderRepository.php
@@ -35,7 +35,7 @@ class LocationReaderRepository
 
 
         $locationData = new LocationData();
-        $locationData->id = isset($row->id) ? (int)$row->id : null;
+        $locationData->id = isset($row->id) ? (string)$row->id : null;
         $locationData->osm_id = isset($row->osm_id) ? $row->osm_id : null;
         $locationData->country = isset($row->country['en']) ? (string)$row->country['en'] : null;
         $locationData->countryDefault = isset($row->country['default']) ? (string)$row->country['default'] : null;

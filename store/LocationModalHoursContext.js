4f999d6e5ab6b1bf02572b63a2f82f97518f70db
M-Shahbaz <mr.shahbaz.aslam@gmail.com>
2021-08-31 18:28:53 +0500

    save hours

--- store/LocationModalHoursContext.js
+++ store/LocationModalHoursContext.js
@@ -1,15 +1,13 @@
 import React from 'react';
 
 const LocationModalHoursContext = React.createContext({
-    hours: {
-        monday: { from: '', to: '' },
-        tuesday: { from: '', to: '' },
-        wednesday: { from: '', to: '' },
-        thursday: { from: '', to: '' },
-        friday: { from: '', to: '' },
-        saturday: { from: '', to: '' },
-        sunday: { from: '', to: '' },
-    }
+    monday: { from: '', to: '' },
+    tuesday: { from: '', to: '' },
+    wednesday: { from: '', to: '' },
+    thursday: { from: '', to: '' },
+    friday: { from: '', to: '' },
+    saturday: { from: '', to: '' },
+    sunday: { from: '', to: '' },
 });
 
 export default LocationModalHoursContext;

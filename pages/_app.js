38b2c5b4002776ab4d9d9ebf7491b7224a69543a
M-Shahbaz <mr.shahbaz.aslam@gmail.com>
2021-11-18 03:49:27 +0500

    StylesProvider generateClassName

--- pages/_app.js
+++ pages/_app.js
@@ -13,6 +13,11 @@ import { useStore } from "store/index.js";
 
 import "styles/scss/nextjs-material-kit.scss?v=1.2.0";
 
+import { StylesProvider, createGenerateClassName } from '@material-ui/core/styles';
+
+const generateClassName = createGenerateClassName({
+  productionPrefix: 'cjss',
+});
 
 Router.events.on("routeChangeStart", (url) => {
   console.log(`Loading: ${url}`);
@@ -64,30 +69,32 @@ export default class MyApp extends App {
     const store = useStore(pageProps.initialReduxState)
 
     return (
-      <ReduxProvider store={store}>
-        <Provider
-          // Provider options are not required but can be useful in situations where
-          // you have a short session maxAge time. Shown here with default values.
-          options={{
-            // Client Max Age controls how often the useSession in the client should
-            // contact the server to sync the session state. Value in seconds.
-            // e.g.
-            // * 0  - Disabled (always use cache value)
-            // * 60 - Sync session state with server if it's older than 60 seconds
-            clientMaxAge: 0,
-            // Keep Alive tells windows / tabs that are signed in to keep sending
-            // a keep alive request (which extends the current session expiry) to
-            // prevent sessions in open windows from expiring. Value in seconds.
-            //
-            // Note: If a session has expired when keep alive is triggered, all open
-            // windows / tabs will be updated to reflect the user is signed out.
-            keepAlive: 0
-          }}
-          session={pageProps.session}
-        >
-          <Component {...pageProps} />
-        </Provider>
-      </ReduxProvider>
+      <StylesProvider generateClassName={generateClassName}>
+        <ReduxProvider store={store}>
+          <Provider
+            // Provider options are not required but can be useful in situations where
+            // you have a short session maxAge time. Shown here with default values.
+            options={{
+              // Client Max Age controls how often the useSession in the client should
+              // contact the server to sync the session state. Value in seconds.
+              // e.g.
+              // * 0  - Disabled (always use cache value)
+              // * 60 - Sync session state with server if it's older than 60 seconds
+              clientMaxAge: 0,
+              // Keep Alive tells windows / tabs that are signed in to keep sending
+              // a keep alive request (which extends the current session expiry) to
+              // prevent sessions in open windows from expiring. Value in seconds.
+              //
+              // Note: If a session has expired when keep alive is triggered, all open
+              // windows / tabs will be updated to reflect the user is signed out.
+              keepAlive: 0
+            }}
+            session={pageProps.session}
+          >
+            <Component {...pageProps} />
+          </Provider>
+        </ReduxProvider>
+      </StylesProvider>
     )
   }
 }

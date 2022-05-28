c9312b75af9e7c4ae542543d2e0e3c703ae8129b
M-Shahbaz <mr.shahbaz.aslam@gmail.com>
2022-05-28 23:10:11 +0500

    async

--- components/Ad/AdMain.js
+++ components/Ad/AdMain.js
@@ -4,16 +4,6 @@ const AdMain = props => {
 
     return (
         <ins className="widgetsbymahimeta" id="mMTag_News_29474807" data-type="news" data-color="blue" style={{ display: "block", width: "100%" }}>
-            <script
-                dangerouslySetInnerHTML={{
-                    __html: `
-              var cachebuster = Math.round(new Date().getTime() / 1000); 
-              var mMTagScript = document.createElement('script'); 
-              mMTagScript.src = '//mahimeta.com/networks/tag.js?cache='+cachebuster; 
-              document.getElementsByTagName("head")[0].appendChild(mMTagScript);
-                `,
-                }}
-            />
         </ins>
     );
 

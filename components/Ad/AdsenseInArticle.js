669bf95a9939e3ca1212dfa99f3e3c1ed8abb4bc
M-Shahbaz <mr.shahbaz.aslam@gmail.com>
2022-06-09 15:07:44 +0500

    AdsenseInArticle

--- components/Ad/AdsenseInArticle.js
+++ components/Ad/AdsenseInArticle.js
@@ -2,10 +2,6 @@ import Reactl, { useEffect } from 'react';
 import Script from 'next/script'
 
 const AdsenseInArticle = props => {
-    useEffect(() => {
-        (window.adsbygoogle = window.adsbygoogle || []).push({});
-    }, [])
-
 
     return (
         <div>
@@ -15,10 +11,10 @@ const AdsenseInArticle = props => {
                 data-ad-format="fluid"
                 data-ad-client="ca-pub-4483485667832613"
                 data-ad-slot="5804681160">
-                <Script
-                    src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-4483485667832613`}
-                    strategy="afterInteractive"
-                ></Script></ins>
+            </ins>
+            <script>
+                (adsbygoogle = window.adsbygoogle || []).push({ });
+            </script>
         </div>
     );
 
